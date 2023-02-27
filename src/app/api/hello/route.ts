17c67d534fe05abcdabdce1a5e4c83d66d5b24a8
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-02-27 23:16:53 +0530

    migrated to next 13.2

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,7 @@
+export async function GET(request: Request) {
+  return new Response("Hello world!");
+}
+
+// export async function POST(request: Request) {
+//   return new Response("Hello world!from post");
+// }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
+
   return (
     <div className={`grid bg-black w-full h-screen place-items-center`}>
-      <h1 className="font-montserrat text-3xl text-red-500">Turbopack</h1>
+      <h1 className="font-montserrat text-3xl text-red-500">tailwind+turbopack</h1>
     </div>
   );
 }
